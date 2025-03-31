57dddacfcad046422ab3b642ea5a221fe716f929
dhiren-rajesh <dhirenrajeshk@gmail.com>
2025-03-31 19:47:27 +0530

    Starts on top on refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
+import { useEffect } from "react";
 import Introduction from "../components/Introduction/Introduction";
 import Banner from "../components/Banner/Banner";
 import Header from "../components/Header/Header";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="max-w-screen">
       <Header />
